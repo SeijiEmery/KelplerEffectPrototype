c80a4aea6601d22c8100ec05928d1345d359e65b
eli23 <funbaba@live.com>
2019-11-08 09:33:56 +0800

    Added Horizontal Ship/Bullet Prototype.

---
+++ Assets/BulletMovement.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletMovement : MonoBehaviour
+{
+    public float speed = 2f;
+    void Start()
+    {
+        GameObject[] redShips = GameObject.FindGameObjectsWithTag("RedShip");
+        foreach (GameObject redship in redShips)
+        {
+            Physics.IgnoreCollision(redship.GetComponent<Collider>(), GetComponent<Collider>());
+        }
+    }
+    void FixedUpdate()
+    {
+        transform.Translate(Vector3.up * Time.deltaTime * speed);
+    }
+    void OnCollisionEnter(Collision collision)
+    {
+        //if (!(collision.gameObject.tag == "RedShip"))
+        //{
+            Destroy(gameObject);
+       // }
+    }
+}
