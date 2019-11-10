ba0e8d43375f8382dc8e9e305be6f458fdb15962
ColaDev <funbaba@live.com>
2019-11-10 02:58:37 -0800

    Applied crab barrel rotation with MoveY and optimized some trivial code.

--- Assets/TestScript/CrabTestController.cs
+++ Assets/TestScript/CrabTestController.cs
@@ -11,12 +11,11 @@ public class CrabTestController : MonoBehaviour
     public float firingPower = 8.0f;                             //Set firing power
     public float cooldown_in_seconds = 2.0f;            //Set firing rate by cooldown
     public GameObject MortarShell;                          //Bullet type is mortar
-    private GameObject barrel;                                   //Crab Mortar Barrel tracking aim
+    public GameObject barrel;                                   //Crab Mortar Barrel tracking aim
     private Transform barreltf;
     private float shootingGap = 0f;
     void Start()
     {
-        barrel = this.transform.Find("Barrel").gameObject;
         barreltf = barrel.GetComponent<Transform>();
     }
     void FixedUpdate()
@@ -25,33 +24,21 @@ public class CrabTestController : MonoBehaviour
         var movement = Vector3.forward * Input.GetAxis("MoveZ") + Vector3.right * Input.GetAxis("MoveX");
         transform.Translate(movement * speed * Time.deltaTime, Space.World);
 
+        //Simple Rotation
         if (Input.GetKey(KeyCode.Q))
             transform.Rotate(Vector3.down * rotateSpeed, Space.Self);
         if (Input.GetKey(KeyCode.E))
             transform.Rotate(Vector3.up * rotateSpeed, Space.Self);
-        /*if (Input.GetKey(KeyCode.Tab))
-        {
-            if(barreltf.localRotation.eulerAngles.x <= 300f && barreltf.localRotation.eulerAngles.x > 0f)
-            {
-                barreltf.localRotation = Quaternion.Euler(new Vector3(-60f,0f, 0f));
-            }
-            else
-                barreltf.Rotate(Vector3.left * barrelSpeed, Space.Self);
-        }
 
-        if (Input.GetKey(KeyCode.LeftShift))
-        {
-            if (barreltf.localRotation.eulerAngles.x >= 0f && barreltf.localRotation.eulerAngles.x<300)
-            {
-                barreltf.localRotation = Quaternion.Euler(new Vector3(0f, 0f, 0f));
-            }
-            else
-                barreltf.Rotate(Vector3.right, Space.Self);
-        }*/
+        //Barrel angle adjustment
+        if (barreltf.localRotation.eulerAngles.z >= 60f && barreltf.localRotation.eulerAngles.z <= 210f)
+            barreltf.localRotation = Quaternion.Euler(new Vector3(0f, -90f, 60f));
+        else if (barreltf.localRotation.eulerAngles.z > 210f && barreltf.localRotation.eulerAngles.z < 360f)
+            barreltf.localRotation = Quaternion.Euler(new Vector3(0f, -90f, 0f));
+        barreltf.Rotate(Vector3.forward * barrelSpeed * Input.GetAxis("MoveY"), Space.Self);
 
-
-        //Start shooting if time gap is enough.
-        if (shootingGap < cooldown_in_seconds)
+            //Start shooting if time gap is enough.
+            if (shootingGap < cooldown_in_seconds)
         {
             shootingGap += Time.deltaTime;
         }
@@ -59,16 +46,11 @@ public class CrabTestController : MonoBehaviour
         {
             shootingGap = 0f;
             //Shoot
-            var clone = Instantiate(MortarShell, barreltf.position + 0.5f * barreltf.forward, Quaternion.Euler(1,1,1));
+            var clone = Instantiate(MortarShell, barreltf.position + 0.5f * barreltf.right, Quaternion.Euler(1,1,1));
             clone.GetComponent<MortarMovement>().speed = bulletSpeed;
             clone.GetComponent<MortarMovement>().firingPower = firingPower;
-            clone.GetComponent<MortarMovement>().shootingPos = barreltf.forward;
+            clone.GetComponent<MortarMovement>().shootingPos = barreltf.right;
         }
 
-    }
-    void OnCollisionExit(Collision other)
-    {
-        //Solving weird velocity upwards when pressed to the ground and moved a bit, but still glitchy
-        GetComponent<Rigidbody>().velocity = Vector3.zero;
     }
 }
