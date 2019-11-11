7f29b6bdb429a6f6adc6d58f3089a063e60b31eb
Brian Zhao <zhao.brian.w@gmail.com>
2019-11-11 11:53:01 -0800

    Unit Selection
    Select units with Mouse Selector via Unit Manager.  Currently always additive selection.
    
    Maybe we want to add ToggleSelection to the Selection Manager

---
+++ Assets/Script/PlayerControls/MouseSelector.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseSelector : MonoBehaviour
+{
+    private UnitSelectionManager selectionManager;
+
+    [SerializeField]
+    new private Camera camera;
+
+    private GameObject lastHit;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        selectionManager = GetComponent<UnitSelectionManager>();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 mousePos = Input.mousePosition;
+        Ray ray = camera.ScreenPointToRay(mousePos);
+        RaycastHit hit;
+        if (lastHit != null)
+        {
+            UnhighlightObject(lastHit);
+            lastHit = null;
+        }
+        if (Physics.Raycast(ray, out hit))
+        {
+            lastHit = hit.collider.gameObject;
+            HighlightObject(lastHit);
+            SelectableUnit unit = lastHit.GetComponent<SelectableUnit>();
+            if (unit != null && Input.GetMouseButtonDown(0))
+            {
+                if (unit.selected)
+                    selectionManager.RemoveUnitFromSelection(unit);
+                else
+                    selectionManager.AddUnitToSelection(unit);
+            }
+        }
+    }
+
+    public void HighlightObject(GameObject go)
+    {
+        var selectable = go.GetComponent<SelectableUnit>();
+        if (selectable != null)
+        {
+            selectable.highlighted = true;
+        }
+    }
+    void UnhighlightObject(GameObject go)
+    {
+        var selectable = go.GetComponentInParent<SelectableUnit>();
+        if (selectable != null)
+        {
+            selectable.highlighted = false;
+        }
+    }
+}
