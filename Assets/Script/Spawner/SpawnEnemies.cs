9948d3218826098e674d64183388832a81dec310
Jordan Lee <50028638+SpicyMeatloaf@users.noreply.github.com>
2019-11-12 18:47:45 -0800

    Adding SpawnEnemy script and example usage scene

---
+++ Assets/Script/Spawner/SpawnEnemies.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnEnemies : MonoBehaviour {
+
+	public GameObject theEnemy;
+	public int xPos;
+	public int yPos;
+	public int zPos;
+	public int enemyCount;
+
+	void Start() {
+		while (enemyCount < 10)
+		{
+			xPos = Random.Range(1,5);
+			yPos = Random.Range(0,1);
+			zPos = Random.Range(1, 6);
+			Instantiate(theEnemy, new Vector3(xPos,yPos,zPos), Quaternion.identity);
+			enemyCount += 1;
+		}
+
+	}
+
+	void Update(){
+	}
+}
