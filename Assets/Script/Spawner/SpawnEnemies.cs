56f504c782c4d3a2cd8366a525ab6406ec2d71dc
jcuneo <jcuneo@ucsc.edu>
2019-11-13 18:46:11 -0800

    EnemyMovement and updated EnemySpawning

--- Assets/Script/Spawner/SpawnEnemies.cs
+++ Assets/Script/Spawner/SpawnEnemies.cs
@@ -9,17 +9,17 @@ public class SpawnEnemies : MonoBehaviour {
 	public int yPos;
 	public int zPos;
 	public int enemyCount;
+	public int totalEnemies = 10;
 
 	void Start() {
-		while (enemyCount < 10)
+		while (enemyCount < totalEnemies)
 		{
-			xPos = Random.Range(1,5);
-			yPos = Random.Range(0,1);
-			zPos = Random.Range(1, 6);
+			xPos = Random.Range(1,15);
+			yPos = Random.Range(0,25);
+			zPos = Random.Range(1,6);
 			Instantiate(theEnemy, new Vector3(xPos,yPos,zPos), Quaternion.identity);
 			enemyCount += 1;
 		}
-
 	}
 
 	void Update(){
